46b8d17754b2b5d2066b8836c6666626500a3a04
SebasUrbina <sebastian.urbina@live.cl>
2026-01-18 22:30:18 -0300

    ✨feat: make statefull description input

--- lib/features/quick_entry/widgets/description_input.dart
+++ lib/features/quick_entry/widgets/description_input.dart
@@ -1,15 +1,41 @@
 import 'package:flutter/material.dart';
 
-class DescriptionInput extends StatelessWidget {
+class DescriptionInput extends StatefulWidget {
   final ValueChanged<String> onChanged;
+  final String? initialValue;
 
-  const DescriptionInput({super.key, required this.onChanged});
+  const DescriptionInput({
+    super.key,
+    required this.onChanged,
+    this.initialValue,
+  });
+
+  @override
+  State<DescriptionInput> createState() => _DescriptionInputState();
+}
+
+class _DescriptionInputState extends State<DescriptionInput> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.initialValue ?? '');
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final colors = theme.colorScheme;
+
     return TextField(
+      controller: _controller,
       decoration: InputDecoration(
         hintText: 'Descripción...',
         hintStyle: TextStyle(color: colors.onSurface.withValues(alpha: 0.4)),
@@ -24,7 +50,7 @@ class DescriptionInput extends StatelessWidget {
           color: colors.primary.withValues(alpha: 0.7),
         ),
       ),
-      onChanged: onChanged,
+      onChanged: widget.onChanged,
     );
   }
 }
